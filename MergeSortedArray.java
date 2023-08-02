9424b72e5d949336af796182bfbd54b485ebbf24
Ali Aftab Muhammad <aliaftab9@hotmail.com>
2023-08-02 17:33:07 -0400

    updates to leetcode repo

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -13,12 +13,10 @@ class MergeSortedArray {
     }
     public static void main(String[] args){
         MergeSortedArray msa = new MergeSortedArray();
-        int[] nums1 = {1,2,3,0,9,8,9};
-        int[] nums2 = {2,5,6,5,9,9,0}; 
-        msa.merge(nums1,5,nums2,5);
-        System.out.println(Arrays.toString(nums1));
-        System.out.println(Arrays.toString(nums2));
+            int[] nums1 = {1,2,3,0,9,8,9};
+            int[] nums2 = {2,5,6,5,9,9,0}; 
+            msa.merge(nums1,5,nums2,5);
+            System.out.println(Arrays.toString(nums1));
+            System.out.println(Arrays.toString(nums2));
     }
 }
-
-r
---
+++ ValidPhoneNumbers.java
@@ -0,0 +1,11 @@
+class solution{
+
+
+public double validPhoneNumbers(String[] args){
+    double formula = grep -P '^(\d{3}-|\(\d{3}\) )\d{3}-\d{4}$' file.txt
+
+    
+    return double;
+
+    }
+}
