55435f1337ba94b93c3a99a8d2f79026b6637f9a
Ali Aftab Muhammad <aliaftab9@hotmail.com>
2023-08-04 15:02:33 -0400

    slight updates

--- Search2DMatrix.java
+++ Search2DMatrix.java
@@ -1,35 +1,20 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int[][] twoDMatrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22}, {10,13,14,17,24},{18,21,23,26,30}};
-        int val = 5;
-        
-        boolean found = true;
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+        int i = matrix.length -1;
+        int j = matrix[0].length -1;
+        if(target > matrix[i][j] || target < matrix[0][0] ) return false;
 
-
-        for(int i = 0; i < twoDMatrix.length; i++){
-            for(int j = 0; j < matrix[i].length; j++){
-                if(matrix[i][j] == val){
-                    found = true;
-                    break;
-                }
-                else{
-                    found = false;
-                }
+        int x = 0;
+        int y = j;
+        while(x <= i&& y>=0){
+            if(target == matrix[x][y]) return true;
+            else if( target < matrix[x][y]){
+                y--;
+            }else{
+                x++;
             }
         }
-        return found;
-    }
-}
-
-class solution{
-    public boolean searchMatrix(int[][] M, int T){
-        int y = M.length, i = 0, j = M[i].length - 1;
-        while (i < y && j >= 0) {
-            int cell = M[i][j];
-            if (cell == T) return true;
-            else if (cell > T) j--;
-            else i++;
-        }
         return false;
-    }
+}
 }
